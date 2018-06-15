82e239993fbfe420ae19faaddfee9f3c2adca57c
Changmin Lim <chang.lim92@gmail.com>
2018-06-15 09:19:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,23 @@
-class Song
-  attr_accessor :name, :artist
+class Song 
+  attr_accessor :name, :artist 
   @@all = []
-
-  def initialize(name = '', artist = '')
+  def initialize(name, artist = nil)
     @name = name
-    @@all << self
-    @artist = artist
-  end
+    @artist = artist 
+    @@all < self 
+  end 
+  def self.all 
+    @@all 
+  end 
+  
+  def artist_name 
+    if self.artist != nil 
+      self.artist.name 
+    else 
+      nil 
+    end 
+  end 
+  
 
-  def self.all
-    @@all
-  end
-
-  def artist_name
-    if @artist == ''
-      nil
-    else
-      self.artist.name
-    end
-  end
-end
+  
+end 
